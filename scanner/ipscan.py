ea553aba26b2ec08a0a72994ed83db1bd462f6e4
Neki3141 <trungkien14072007@gmail.com>
2026-03-01 09:52:49 +0100

    done ip scanner

--- scanner/ipscan.py
+++ scanner/ipscan.py
@@ -1,27 +1,28 @@
-import socket
-import os
+import ipaddress
+import subprocess 
+from concurrent.futures import ThreadPoolExecutor
+
 
 
 def sent_icmp(ip):
-      # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    pass 
-
+    ping = subprocess.run(["ping", "-c", "2", str(ip)], capture_output=True)
+    if "100% packet loss" not in ping.stdout.decode():
+        print(str(ip))
+    return
 
 def main():
     print("WELCOME TO IP SCANNER:")
-    try:
-        s1 = int(input("Enter "))
+    ip1 = input("Enter ip1 (format: x.x.x.x):")
+    ip2 = input("Enter ip2 (format: x.x.x.x):")
+
+
+    start_ip = ipaddress.IPv4Address(ip1)
+    end_ip   = ipaddress.IPv4Address(ip2)
+
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        for ip in range(int(start_ip), int(end_ip) + 1):
+            executor.submit(sent_icmp, ipaddress.IPv4Address(ip))
 
 if __name__ == ("__main__"):
     main()
-
-
-
-# import os
-# hostname = "127.0.0."
-# for i in range(0,20):
-#     hostname_x = hostname + str(i)
-#     response = os.system("ping -c 1 " + hostname_x)
-#     if response == 0:
-#         print("nice" + str(i))
     
